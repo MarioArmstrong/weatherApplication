9b7b5ed11c0adaae4f12ceed429c587406e99ce0
MarioArmstrong <arms.mario@icloud.com>
2022-07-27 21:10:05 -0800

    updated the js to include dynamic html elements, but still broken

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,63 @@
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+var APIKey = "59ce11a5925422e0542cfcb16e4281b7";
 
-var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=San Diego,us&appid=59ce11a5925422e0542cfcb16e4281b7";
-// var APIKey = "59ce11a5925422e0542cfcb16e4281b7";
+var input = document.getElementById("searchBoxInput");
 var cityBtn = document.getElementById("searchCityBTN");
-var searchBox = document.getElementById("searchBox");
-
 var cityHighlight = document.querySelector(".city");
+var rows = document.querySelector("row");
 
-var forecastDay1 = document.querySelector(".forecastDay1");
-var forecastDay2 = document.querySelector(".forecastDay2");
-var forecastDay3 = document.querySelector(".forecastDay3");
-var forecastDay4 = document.querySelector(".forecastDay4");
-var forecastDay5 = document.querySelector(".forecastDay5");
+const Fahrenheit = ((kelvin - 273.15) * 1.8) + 32
 
+function fetchURLInfo(event) {
+    event.preventDefault();
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input.value + ",us&appid=59ce11a5925422e0542cfcb16e4281b7";
 
-fetch(requestURL)
-.then(function(response){
-    return response.json();// converts to JSON object
-})
-
-.then(function(data) {
-    // console.log(data);
-    for(var i=0; i< data.length; i++){
-        var createTableRow = document.createElement("tr");
-        var tableData = document.createElement("td")
-
-    }
-});
+    fetch(requestURL)
+    .then(function(response){//.then is a promise - has to be run after the response
+        return response.json();// converts to JSON object - parses data so we can use it below
+    })
+    
+    .then(function(data) {
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        console.log(data);
+        // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+        // ******onecall gets you all the temp data you need******
 
 
 
+        var urlData = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+            fetch(urlData)
+            .then(function(response){
+                return response.json();
+            })
 
+            .then(function(data){
+                console.log(data);
+                for(var i=0; i< 6; i++){
+
+                    var createCardSection = document.createElement("div")
+                    var createCardHeading = document.createElement("h3");            
+                    var cardContent = document.createElement("p");
+            
+                    cardContent.textContent = data.daily[i].temp.max;
+                    // createCardHeading.textContent = data.name;
+
+                    createCardHeading.appendChild(cardContent);
+                    createCardSection.appendChild(createCardHeading);
+                    rows.appendChild(createCardSection);            
+
+                    cardContent.classList.add("card-text");
+                    createCardHeading.classList.add("card-title");
+                    createCardSection.classList.add("card-body");
+
+                }
+            })
+
+
+    });
+    
+}
                     //EVENT LISTENERS
 
-cityBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    console.log("search button was clicked!");
-});
+cityBtn.addEventListener("click",fetchURLInfo);
